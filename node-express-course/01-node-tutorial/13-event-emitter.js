0d041880578263b4a56b7cff0f4be44ded47f8a8
dwskme <officialsamirkc@gmail.com>
2022-06-13 17:43:05 +0545

    Remaning basics

--- node-express-course/01-node-tutorial/13-event-emitter.js
+++ node-express-course/01-node-tutorial/13-event-emitter.js
@@ -14,8 +14,8 @@ customEmitter.on('response', (name, id) => {
   console.log(`data recieved user ${name} with id:${id}`)
 })
 
-customEmitter.on('response', (name) => {
-  console.log(`some other logic here ${name}`)
+customEmitter.on('response', () => {
+  console.log('some other logic here')
 })
 
 customEmitter.emit('response', 'john', 34)
